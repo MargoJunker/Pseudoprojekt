ff3263bb0fb969a7140a49a369f0177eeb0c7aee
Margo Junker <margo.junker@khk.ee>
2015-09-10 12:25:03 +0300

    Lisa dokumenti pilt ilusast kassist ja tee nii, et kui pilti klikitakse, siis muutub kassipilt koerapildiks [Delivers #103070244]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -3,10 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <script>
+        document.addEventListener("click",function(){
+            document.getElementById("pic").src="http://petapixel.com/assets/uploads/2013/06/vtWFwyj.jpg";
+        });
+    </script>
 </head>
 <body>
-    <button onclick="alert('Tere Maailm!')">Tere Maailm</button><br>
-    <a href="http://www.khk.ee" onclick="alert('Tere Maailm');">Tere Maailm</a><br>
-    <a href="" onclick="alert('J‰‰me siia!');">J‰‰me Siia</a><br>
+<button onclick="alert('Tere Maailm!')">Tere Maailm</button><br>
+<a href="http://www.khk.ee" onclick="alert('Tere Maailm');">Tere Maailm</a><br>
+<a href="" onclick="alert('J‰‰me siia!');">J‰‰me Siia</a><br>
+
+<img id="pic" width="200" height="235" src="http://www.thedailycat.com/health/advances/cat_identification/images/large.jpg">
 </body>
 </html>
