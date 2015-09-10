14039786ddd246be11add803f0104b9feb18fec4
Margo Junker <margo.junker@khk.ee>
2015-09-10 12:14:18 +0300

    Loo body osasse link tekstiga "jääme siia", millele klikkides kuvab brauser teadet "jääme siia" ega ei navigeeri antud lehelt ära [Delivers #103070218]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -5,7 +5,8 @@
     <title>Document</title>
 </head>
 <body>
-    <button onclick="alert('Tere Maailm!')">Tere Maailm</button>
-    <a href="http://www.khk.ee" onclick="alert('Tere Maailm');">Tere Maailm</a>
+    <button onclick="alert('Tere Maailm!')">Tere Maailm</button><br>
+    <a href="http://www.khk.ee" onclick="alert('Tere Maailm');">Tere Maailm</a><br>
+    <a href="" onclick="alert('Jääme siia!');">Jääme Siia</a>
 </body>
 </html>
