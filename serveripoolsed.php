438a37f9da32b4a726fb67768a8d1bcfa2b241fc
Margo Junker <margo.junker@khk.ee>
2015-09-10 11:31:02 +0300

    Teha kaks muutujat väärtustega 1.23 ja 2.34 ning kuvada ekraanil nende summat [Delivers #103064372]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -1,7 +1,12 @@
 <?php
 
 $isik=(object) array('eesnimi'=>'Margo', 'perenimi'=>'Junker','vanus'=>21,'sugu'=>'Mees');
-$eesnimi_algab_vokaaliga = strstr("AEIOUÕÄÜ",$isik->{'eesnimi'}[0]);
+$eesnimi_algab_vokaaliga = strstr("AEIOUÕÄÖÜ",$isik->{'eesnimi'}[0]);
+
+$arv1=1.23;
+$arv2=2.34;
+$summa=$arv1+$arv2;
+echo "Arvude ".$arv1." ja ".$arv2." summa on ".$summa."<br>";
 
 echo $isik->{'eesnimi'}." ".$isik->{'perenimi'}." (".$isik->{'vanus'}.") ".$isik->{'sugu'}."<br>";
 
