e27946549bcd0e87a61885a03fd26f883d39f6a6
Margo Junker <margo.junker@khk.ee>
2015-09-10 12:03:45 +0300

    Loo fail kliendipoolsed.php ja loo sinna sisse HTML5 boilerplate [Delivers #103070028]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -1,10 +1,10 @@
 <!doctype html>
-<html lang=ee>
+<html lang="ee">
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
 <body>
-    <button onclick="alert('Tere Maailm!')">Tere Maailm</button>
+
 </body>
 </html>
