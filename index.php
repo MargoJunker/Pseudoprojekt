8a203d715a438c780a9f2e5106b03c2279156df3
Margo Junker <margo.junker@khk.ee>
2015-09-15 12:27:18 +0300

    Luua fail nimega index.php. Lisada sinna html5 boilerplate. <body> osasse lisada PHP märgendi vahele kontroll, kas eksisteerib muutuja nimega $_GET['page'] ja kui eksisteerib, siis require'ida fail, mille nimeks on selle $_GET['page'] sisu pluss string ".php". Näiteks kui $_GET['page'] väärtuseks oli home, siis requiretakse home.php. [Delivers #103299104]

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!doctype html>
-<html lang="et">
+<html lang="ee">
 <head>
     <meta charset="UTF-8">
     <title>Pseudoprojekt</title>
 </head>
+
 <body>
-<h1>Tere maailm!</h1>
-<hr>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque beatae doloremque in quod. Consequuntur dolorem
-    fugiat maxime modi quam quasi quidem sint? Adipisci dicta dignissimos dolores eos incidunt ipsam magnam quam
-    recusandae sed, unde. Deleniti, error laborum maxime minima nulla possimus quidem temporibus! Accusamus autem,
-    blanditiis consectetur cupiditate delectus dignissimos ea enim et ex expedita incidunt ipsa ipsam non numquam omnis
-    quas quia ratione repellat sequi sit veniam vero, vitae. Ab ad cum dolore dolorum earum error est fugiat incidunt
-    inventore obcaecati odit officiis quo rerum sapiente tempora tempore vitae, voluptatum! Doloribus facilis laudantium
-    porro repellendus tempora? Explicabo quis, ratione.</p>
+    <?php
+    if(isset($_GET['page']) && file_exists($_GET['page'].".php")){
+        require $_GET['page'].".php";
+    };
+    ?>
+    <p>sakfnsoadng</p>
+</body>
+
+
 </html>
