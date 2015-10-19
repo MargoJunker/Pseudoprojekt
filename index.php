794bcf7d13e8d0c2cfe4ea7d7227d8844b3c474a
Margo Junker <margo.junker@khk.ee>
2015-10-19 09:17:27 +0300

    28. Fake it until you make it [Delivers #103744060]

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require('config.php');
 $db=mysqli_connect(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_DATABASE) or die(mysqli_error($db));
 mysqli_query($db,"SET NAMES 'utf8'");
-$q= mysqli_query($db,"SELECT * FROM posts JOIN authors ON posts.author_id=authors.authors_id");
+$q= mysqli_query($db,"SELECT * FROM $posts JOIN authors ON posts.author_id=authors.authors_id");
 while($row=mysqli_fetch_assoc($q)){
     $posts[]=$row;
 }
